12f4ff9057447ee76527b7d385f0c44f2e84324a
unknown <wlghd0280@gmail.com>
2024-11-01 08:58:02 +0900

    card_action

--- Web/script.js
+++ Web/script.js
@@ -1,6 +1,8 @@
+// script.js
+
 let currentPage = 0;
 const cardsPerPage = 20;
-const totalCards = 40;  // 총 카드 수
+const totalCards = 40;
 const totalPages = Math.ceil(totalCards / cardsPerPage);
 
 // 페이지 로드 시 카드 생성
@@ -10,13 +12,16 @@ window.onload = function() {
     for (let p = 0; p < totalPages; p++) {
         const page = document.createElement('div');
         page.className = 'page';
-        page.style.display = p === 0 ? 'grid' : 'none';  // 첫 번째 페이지만 보이도록 설정
+        page.style.display = p === 0 ? 'grid' : 'none';
         
         for (let i = 1 + p * cardsPerPage; i <= cardsPerPage * (p + 1) && i <= totalCards; i++) {
             const card = document.createElement('div');
             card.className = 'card';
             card.dataset.id = i;
             card.onclick = () => revealGame(card, `game${i}`);
+            
+            const cardInner = document.createElement('div');
+            cardInner.className = 'card-inner';
             
             const cardFront = document.createElement('div');
             cardFront.className = 'card-front';
@@ -29,8 +34,9 @@ window.onload = function() {
             img.alt = "해결된 이미지";
             cardBack.appendChild(img);
             
-            card.appendChild(cardFront);
-            card.appendChild(cardBack);
+            cardInner.appendChild(cardFront);
+            cardInner.appendChild(cardBack);
+            card.appendChild(cardInner);
             page.appendChild(card);
         }
         
@@ -80,6 +86,9 @@ function solveGame() {
 
     if (card) {
         card.classList.add('solved');
+        const cardInner = card.querySelector('.card-inner');
+        cardInner.style.transform = "rotateY(180deg)";
+        
         const cardBack = card.querySelector('.card-back');
         cardBack.innerHTML = `<img src="monster_image${cardId}.jpg" alt="몬스터 이미지" style="width:100%; height:100%;">`;
     }
